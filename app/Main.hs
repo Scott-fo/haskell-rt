04a117c6b9dc1059c5029066af629ecf919261c0
Scott <scottfo@live.com>
2025-01-01 23:22:12 +0000

    render basic image

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+data Scene = Scene
+  { width :: Int,
+    height :: Int
+  }
+  deriving (Show)
+
+renderImage :: Scene -> String
+renderImage s =
+  let w = width s
+      h = height s
+   in unlines [renderPixel i j w h | j <- [0 .. h - 1], i <- [0 .. w - 1]]
+
+renderPixel :: Int -> Int -> Int -> Int -> String
+renderPixel i j w h =
+  let r = fromIntegral i / fromIntegral (w - 1) :: Double
+      g = fromIntegral j / fromIntegral (h - 1) :: Double
+      b = 0.0 :: Double
+      ir = floor (255.999 * r) :: Int
+      ig = floor (255.999 * g) :: Int
+      ib = floor (255.999 * b) :: Int
+   in unwords [show ir, show ig, show ib]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let scene = Scene {width = 256, height = 256}
+      header = "P3\n" ++ show (width scene) ++ " " ++ show (height scene) ++ "\n255\n"
+      body = renderImage scene
+  writeFile "image.ppm" (header ++ body)
