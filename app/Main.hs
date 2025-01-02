4110369cdaaa2e9dc50f06c050b0e623d9bbb98b
Scott <scottfo@live.com>
2025-01-02 14:21:07 +0000

    refactor + add rays

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,56 @@
 module Main where
 
 import Colour (writeColour)
-import Vec3 (Vec3 (Vec3))
-
-data Scene = Scene
-  { width :: Int,
-    height :: Int
-  }
-  deriving (Show)
-
-renderImage :: Scene -> String
-renderImage s =
-  let w = width s
-      h = height s
-   in unlines
-        [ writeColour (Vec3 r g 0)
-          | j <- [0 .. h - 1],
-            i <- [0 .. w - 1],
-            let r = fromIntegral i / fromIntegral (w - 1)
-                g = fromIntegral j / fromIntegral (h - 1)
-        ]
+import Ray (Ray (Ray), rayColour)
+import Vec3 (Vec3 (Vec3), add, div, mul, sub)
 
 main :: IO ()
 main = do
-  let scene = Scene {width = 256, height = 256}
-      header = "P3\n" ++ show (width scene) ++ " " ++ show (height scene) ++ "\n255\n"
-      body = renderImage scene
+  let aspectRatio = 16.0 / 9.0 :: Double
+      imageWidth = 400 :: Double
+      imageHeight = floor (imageWidth / aspectRatio) :: Int
+      focalLength = 1.0 :: Double
+      viewportHeight = 2.0 :: Double
+      viewportWidth = viewportHeight * (imageWidth / fromIntegral imageHeight)
+
+      cameraCenter = Vec3 0 0 0
+      viewportU = Vec3 viewportWidth 0 0
+      viewportV = Vec3 0 (-viewportHeight) 0
+
+      pixelDeltaU = case viewportU `Vec3.div` imageWidth of
+        Just v -> v
+        Nothing -> error "DivideByZero"
+
+      pixelDeltaV = case viewportV `Vec3.div` fromIntegral imageHeight of
+        Just v -> v
+        Nothing -> error "DivideByZero"
+
+      viewportUpperLeft =
+        cameraCenter
+          `sub` Vec3 0 0 focalLength
+          `sub` (viewportU `mul` 0.5)
+          `sub` (viewportV `mul` 0.5)
+
+      pixel00Loc = viewportUpperLeft `add` ((pixelDeltaU `add` pixelDeltaV) `mul` 0.5)
+
+      header = ppmHeader (floor imageWidth) imageHeight
+      body = renderImage (floor imageWidth) imageHeight pixel00Loc pixelDeltaU pixelDeltaV cameraCenter
+
   writeFile "image.ppm" (header ++ body)
+
+renderImage :: Int -> Int -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> String
+renderImage w h pixel00Loc pixelDeltaU pixelDeltaV cameraCenter =
+  unlines
+    [ writeColour (rayColour (Ray cameraCenter rayDirection))
+      | j <- [0 .. h - 1],
+        i <- [0 .. w - 1],
+        let pixelCenter =
+              pixel00Loc
+                `add` (pixelDeltaU `mul` fromIntegral i)
+                `add` (pixelDeltaV `mul` fromIntegral j)
+            rayDirection = pixelCenter `sub` cameraCenter
+    ]
+
+ppmHeader :: Int -> Int -> String
+ppmHeader w h =
+  "P3\n" ++ show w ++ " " ++ show h ++ "\n255\n"
--- src/Ray.hs
+++ src/Ray.hs
@@ -1,6 +1,7 @@
 module Ray where
 
-import Vec3 (Point3, Vec3, add, mul)
+import Colour (Colour)
+import Vec3 (Point3, Vec3 (Vec3), add, mul, unitVector)
 
 data Ray = Ray
   { origin :: Point3,
@@ -9,3 +10,13 @@ data Ray = Ray
 
 at :: Ray -> Double -> Vec3
 at (Ray o d) t = add o (mul d t)
+
+rayColour :: Ray -> Colour
+rayColour (Ray _ d) =
+  case Vec3.unitVector d of
+    Just (Vec3 _ y _) ->
+      let a = 0.5 * (y + 1.0)
+          white = Vec3 1.0 1.0 1.0
+          blue = Vec3 0.5 0.7 1.0
+       in Vec3.add (Vec3.mul white (1.0 - a)) (Vec3.mul blue a)
+    Nothing -> Vec3 0 0 0
