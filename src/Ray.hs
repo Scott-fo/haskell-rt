b0117394edd9143133587562c02de66ba52a7491
Scott <scottfo@live.com>
2025-01-02 16:38:59 +0000

    coloured sphere

--- src/Ray.hs
+++ src/Ray.hs
@@ -8,30 +8,42 @@ data Ray = Ray
     direction :: Vec3
   }
 
-at :: Ray -> Double -> Vec3
+at :: Ray -> Double -> Point3
 at (Ray o d) t = add o (mul d t)
 
 rayColour :: Ray -> Colour
 rayColour r =
+  case hitSphere (Vec3 0 0 (-1)) 0.5 r of
+    Just t -> sphereColour r t
+    Nothing -> backgroundColour r
+
+sphereColour :: Ray -> Double -> Vec3
+sphereColour r t =
+  let n = normalize (at r t `sub` Vec3 0 0 (-1))
+   in (n `mul` 0.5) `add` Vec3 0.5 0.5 0.5
+
+backgroundColour :: Ray -> Vec3
+backgroundColour r =
   case unitVector (direction r) of
     Just (Vec3 _ y _) ->
-      if hitSphere (Vec3 0 0 (-1)) 0.5 r
-        then Vec3 1 0 0
-        else
-          let a = 0.5 * (y + 1.0)
-              white = Vec3 1.0 1.0 1.0
-              blue = Vec3 0.5 0.7 1.0
-           in (white `mul` (1.0 - a)) `add` (blue `mul` a)
+      let t = 0.5 * (y + 1.0)
+          white = Vec3 1.0 1.0 1.0
+          blue = Vec3 0.5 0.7 1.0
+       in (white `mul` (1.0 - t)) `add` (blue `mul` t)
     Nothing -> Vec3 0 0 0
 
-hitSphere :: Point3 -> Double -> Ray -> Bool
-hitSphere center radius ray =
-  discriminant center radius ray >= 0
-
-discriminant :: Point3 -> Double -> Ray -> Double
-discriminant center radius (Ray origin' direction') =
+hitSphere :: Point3 -> Double -> Ray -> Maybe Double
+hitSphere center radius (Ray origin' direction') =
   let oc = center `sub` origin'
       a = dot direction' direction'
       b = -(2.0 * dot direction' oc)
       c = dot oc oc - (radius * radius)
-   in b * b - 4 * a * c
+      discriminant = b * b - 4 * a * c
+   in if discriminant < 0
+        then Nothing
+        else Just ((-b - sqrt discriminant) / (2 * a))
+
+normalize :: Vec3 -> Vec3
+normalize v = case unitVector v of
+  Just u -> u
+  Nothing -> Vec3 0 0 0
