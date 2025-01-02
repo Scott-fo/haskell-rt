ad1bb489bd864201785eb479d18ad56e5861d4d0
Scott <scottfo@live.com>
2025-01-02 15:24:53 +0000

    refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -7,49 +7,74 @@ import Vec3 (Vec3 (Vec3), add, div, mul, sub)
 main :: IO ()
 main = do
   let aspectRatio = 16.0 / 9.0 :: Double
-      imageWidth = 400 :: Double
-      imageHeight = floor (imageWidth / aspectRatio) :: Int
+      iw = 400 :: Int
+      ih = floor (fromIntegral iw / aspectRatio) :: Int
       focalLength = 1.0 :: Double
       viewportHeight = 2.0 :: Double
-      viewportWidth = viewportHeight * (imageWidth / fromIntegral imageHeight)
+      viewportWidth = viewportHeight * (fromIntegral iw / fromIntegral ih)
 
-      cameraCenter = Vec3 0 0 0
+      cc = Vec3 0 0 0
       viewportU = Vec3 viewportWidth 0 0
       viewportV = Vec3 0 (-viewportHeight) 0
 
-      pixelDeltaU = case viewportU `Vec3.div` imageWidth of
+      pDU = case viewportU `Vec3.div` fromIntegral iw of
         Just v -> v
         Nothing -> error "DivideByZero"
 
-      pixelDeltaV = case viewportV `Vec3.div` fromIntegral imageHeight of
+      pDV = case viewportV `Vec3.div` fromIntegral ih of
         Just v -> v
         Nothing -> error "DivideByZero"
 
       viewportUpperLeft =
-        cameraCenter
+        cc
           `sub` Vec3 0 0 focalLength
           `sub` (viewportU `mul` 0.5)
           `sub` (viewportV `mul` 0.5)
 
-      pixel00Loc = viewportUpperLeft `add` ((pixelDeltaU `add` pixelDeltaV) `mul` 0.5)
+      pLoc = viewportUpperLeft `add` ((pDU `add` pDV) `mul` 0.5)
 
-      header = ppmHeader (floor imageWidth) imageHeight
-      body = renderImage (floor imageWidth) imageHeight pixel00Loc pixelDeltaU pixelDeltaV cameraCenter
+      renderConfig =
+        RenderConfig
+          { imageWidth = iw,
+            imageHeight = ih,
+            pixel00Loc = pLoc,
+            pixelDeltaU = pDU,
+            pixelDeltaV = pDV,
+            cameraCenter = cc
+          }
+
+      header = ppmHeader iw ih
+      body = renderImage renderConfig
 
   writeFile "image.ppm" (header ++ body)
 
-renderImage :: Int -> Int -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> String
-renderImage w h pixel00Loc pixelDeltaU pixelDeltaV cameraCenter =
-  unlines
-    [ writeColour (rayColour (Ray cameraCenter rayDirection))
-      | j <- [0 .. h - 1],
-        i <- [0 .. w - 1],
-        let pixelCenter =
-              pixel00Loc
-                `add` (pixelDeltaU `mul` fromIntegral i)
-                `add` (pixelDeltaV `mul` fromIntegral j)
-            rayDirection = pixelCenter `sub` cameraCenter
-    ]
+data RenderConfig = RenderConfig
+  { imageWidth :: Int,
+    imageHeight :: Int,
+    pixel00Loc :: Vec3,
+    pixelDeltaU :: Vec3,
+    pixelDeltaV :: Vec3,
+    cameraCenter :: Vec3
+  }
+
+renderImage :: RenderConfig -> String
+renderImage config =
+  let w = imageWidth config
+      h = imageHeight config
+      pixel00 = pixel00Loc config
+      deltaU = pixelDeltaU config
+      deltaV = pixelDeltaV config
+      camera = cameraCenter config
+   in unlines
+        [ writeColour (rayColour (Ray camera rayDirection))
+          | j <- [0 .. h - 1],
+            i <- [0 .. w - 1],
+            let pixelCenter =
+                  pixel00
+                    `add` (deltaU `mul` fromIntegral i)
+                    `add` (deltaV `mul` fromIntegral j)
+                rayDirection = pixelCenter `sub` camera
+        ]
 
 ppmHeader :: Int -> Int -> String
 ppmHeader w h =
