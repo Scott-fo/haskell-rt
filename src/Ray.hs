047616955fc51062d9ea70aad3e2936c694da12a
Scott <scottfo@live.com>
2025-01-02 16:03:08 +0000

    add sphere

--- src/Ray.hs
+++ src/Ray.hs
@@ -1,7 +1,7 @@
 module Ray where
 
 import Colour (Colour)
-import Vec3 (Point3, Vec3 (Vec3), add, mul, unitVector)
+import Vec3 (Point3, Vec3 (Vec3), add, dot, mul, sub, unitVector)
 
 data Ray = Ray
   { origin :: Point3,
@@ -12,11 +12,26 @@ at :: Ray -> Double -> Vec3
 at (Ray o d) t = add o (mul d t)
 
 rayColour :: Ray -> Colour
-rayColour (Ray _ d) =
-  case Vec3.unitVector d of
+rayColour r =
+  case unitVector (direction r) of
     Just (Vec3 _ y _) ->
-      let a = 0.5 * (y + 1.0)
-          white = Vec3 1.0 1.0 1.0
-          blue = Vec3 0.5 0.7 1.0
-       in Vec3.add (Vec3.mul white (1.0 - a)) (Vec3.mul blue a)
+      if hitSphere (Vec3 0 0 (-1)) 0.5 r
+        then Vec3 1 0 0
+        else
+          let a = 0.5 * (y + 1.0)
+              white = Vec3 1.0 1.0 1.0
+              blue = Vec3 0.5 0.7 1.0
+           in (white `mul` (1.0 - a)) `add` (blue `mul` a)
     Nothing -> Vec3 0 0 0
+
+hitSphere :: Point3 -> Double -> Ray -> Bool
+hitSphere center radius ray =
+  discriminant center radius ray >= 0
+
+discriminant :: Point3 -> Double -> Ray -> Double
+discriminant center radius (Ray origin' direction') =
+  let oc = center `sub` origin'
+      a = dot direction' direction'
+      b = -(2.0 * dot direction' oc)
+      c = dot oc oc - (radius * radius)
+   in b * b - 4 * a * c
