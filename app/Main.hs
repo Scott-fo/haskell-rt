b31753a531dfaa9ae13160f4805b0bfe53a001ab
Scott <scottfo@live.com>
2025-01-04 15:55:37 +0000

    refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -26,54 +26,64 @@ main = do
     Left e -> hPutStrLn stderr $ "Error writing to file: " ++ show (e :: IOException)
     Right _ -> putStrLn "Image successfully written to image.ppm"
 
+generateSphereMaterial :: RayM MaterialWrapper
+generateSphereMaterial = do
+  chooseMat <- randomDouble
+  case () of
+    _
+      | chooseMat < 0.8 -> do
+          c1 <- randomColour
+          MaterialWrapper . Lambertian . mulColours c1 <$> randomColour
+      | chooseMat < 0.95 -> do
+          albedo <- randomColourRange 0.5 1
+          fuzz <- randomDoubleRange 0 0.5
+          return $ MaterialWrapper $ Metal albedo fuzz
+      | otherwise ->
+          return $ MaterialWrapper $ Dielectric 1.5
+
+generateSphere :: Double -> Double -> RayM (Maybe Sphere)
+generateSphere a b = do
+  rx <- randomDouble
+  rz <- randomDouble
+  let center' = Vec3 (a + 0.9 * rx) 0.2 (b + 0.9 * rz)
+      comparePoint = Vec3 4 0.2 0
+
+  if Vec3.length (Vec3.sub center' comparePoint) > 0.9
+    then do
+      Just . Sphere center' 0.2 <$> generateSphereMaterial
+    else return Nothing
+
 buildWorld :: RayM HittableList
 buildWorld = do
-  let groundMaterial = MaterialWrapper $ Lambertian $ Colour $ Vec3 0.5 0.5 0.5
-      groundSphere =
+  let groundSphere =
         Sphere
           { center = Vec3 0.0 (-1000) 0,
             radius = 1000.0,
-            material = groundMaterial
+            material = MaterialWrapper $ Lambertian $ Colour $ Vec3 0.5 0.5 0.5
+          }
+
+      sphere1 =
+        Sphere
+          { center = Vec3 0 1 0,
+            radius = 1.0,
+            material = MaterialWrapper $ Dielectric 1.5
+          }
+
+      sphere2 =
+        Sphere
+          { center = Vec3 (-4) 1 0,
+            radius = 1.0,
+            material = MaterialWrapper $ Lambertian $ Colour $ Vec3 0.4 0.2 0.1
+          }
+
+      sphere3 =
+        Sphere
+          { center = Vec3 4 1 0,
+            radius = 1.0,
+            material = MaterialWrapper $ Metal (Colour $ Vec3 0.7 0.6 0.5) 0.0
           }
 
   randomSpheres <-
-    sequence
-      [ do
-          rx <- randomDouble
-          rz <- randomDouble
-          let center' = Vec3 (a + 0.9 * rx) 0.2 (b + 0.9 * rz)
-          let comparePoint = Vec3 4 0.2 0
-
-          if Vec3.length (Vec3.sub center' comparePoint) > 0.9
-            then do
-              chooseMat <- randomDouble
-              sphereMaterial <-
-                if chooseMat < 0.8
-                  then do
-                    c1 <- randomColour
-                    MaterialWrapper . Lambertian . mulColours c1 <$> randomColour
-                  else
-                    if chooseMat < 0.95
-                      then do
-                        albedo <- randomColourRange 0.5 1
-                        fuzz <- randomDoubleRange 0 0.5
-                        return $ MaterialWrapper $ Metal albedo fuzz
-                      else
-                        return $ MaterialWrapper $ Dielectric 1.5
-
-              return $ Just $ Sphere center' 0.2 sphereMaterial
-            else return Nothing
-        | a <- [-11 .. 11],
-          b <- [-11 .. 11]
-      ]
-
-  let material1 = MaterialWrapper $ Dielectric 1.5
-      sphere1 = Sphere (Vec3 0 1 0) 1.0 material1
-
-      material2 = MaterialWrapper $ Lambertian $ Colour $ Vec3 0.4 0.2 0.1
-      sphere2 = Sphere (Vec3 (-4) 1 0) 1.0 material2
-
-      material3 = MaterialWrapper $ Metal (Colour $ Vec3 0.7 0.6 0.5) 0.0
-      sphere3 = Sphere (Vec3 4 1 0) 1.0 material3
+    traverse (uncurry generateSphere) [(a, b) | a <- [-11 .. 11], b <- [-11 .. 11]]
 
   return $ fromList $ groundSphere : sphere1 : sphere2 : sphere3 : catMaybes randomSpheres
